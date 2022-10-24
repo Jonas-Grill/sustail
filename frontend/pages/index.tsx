fc778ccf082377ac6731616f53794ab5e2af96ff
GinaWeber <57496524+GinaWeber@users.noreply.github.com>
2022-10-24 14:52:12 +0200

    Setup React project and first steps

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+      <div>
+        <Head>
+          <title>Next.js TailwindCSS</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+
+        <div className="container mx-auto">
+          <h1 className="text-lg text-center m-4">TailwindUI/Next.js</h1>
+          <p className="bg-green-600">This is a test of the tailwind next integration.</p>
+        </div>
+      </div>
+  )
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'sustail': '#25995c',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
