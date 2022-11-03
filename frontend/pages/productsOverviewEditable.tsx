33725109166b7869c879ed2c0636dec2e2843050
GinaWeber <57496524+GinaWeber@users.noreply.github.com>
2022-11-03 15:21:29 +0100

    Update productsOverviewEditable.tsx

--- frontend/pages/productsOverviewEditable.tsx
+++ frontend/pages/productsOverviewEditable.tsx
@@ -56,8 +56,6 @@ export default function productsOverviewEditable() {
                     <div
                         className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                         {products.map((product) => (
-                            <Link href={product.href}>
-                                <a key={product.id} className="group">
                                     <div
                                         className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                                         <img
@@ -75,8 +73,6 @@ export default function productsOverviewEditable() {
                                     >
                                         Edit product
                                     </button>
-                                </a>
-                            </Link>
                         ))}
                     </div>
                 </div>
