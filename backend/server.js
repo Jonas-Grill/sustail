78b891ed99b3359340e9997e79db1445e98b821b
Jonas-Grill <jonasb.grill@gmail.com>
2022-11-03 12:45:17 +0100

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 require('dotenv').config({path:'./config.env'});
 
 const express = require('express');
+const mongoose = require('mongoose');
+const DATABASE_URL = process.env.DATABASE_URL;
 
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true});
+const database = mongoose.connection;
+
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const errorHandler = require('./middleware/error')
 
 app.use(express.json());
-
-//ErrorHandler (Should be last piece of middleware)
-app.use(errorHandler);
+app.use('/', routes)
 
 const server=app.listen(
     PORT,()=>{
         console.log(`Server is running on port ${PORT}`)
     }
 )
-process.on("unhandledRejection",(error,promise)=>{
+
+process.on("unhandledRejection",(error, promise)=>{
     console.log(`Logged Error: ${error}`);
     server.close(()=>process.exit(1))
-
 })
