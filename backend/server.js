168b6890e2e93fa36480522508fff5dba0b5d9c9
Jonas-Grill <jonasb.grill@gmail.com>
2022-11-17 19:57:28 +0100

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config({path:'./.env'});
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const {failSafeHandler, errorHandler} = require("./middleware/errorHandler");
 const routes = require('./routes');
@@ -15,6 +16,7 @@ const database = mongoose.connection;
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(failSafeHandler);
 app.use(bodyParser.json());
 app.use('/', routes)
